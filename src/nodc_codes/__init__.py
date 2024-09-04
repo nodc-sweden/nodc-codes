3f8f3c1425a04d69f81d1c42541350cc6f8bbcc3
a001985 <magnus.wenzer@smhi.se>
2024-09-04 09:48:34 +0200

    Fix ssl issue

--- src/nodc_codes/__init__.py
+++ src/nodc_codes/__init__.py
@@ -3,6 +3,7 @@ import logging
 import pathlib
 
 import requests
+import ssl
 
 from .translate_codes import TranslateCodes
 
@@ -13,13 +14,13 @@ THIS_DIR = pathlib.Path(__file__).parent
 CONFIG_DIR = THIS_DIR / 'CONFIG_FILES'
 
 CONFIG_URLS = [
-    r'https://raw.githubusercontent.com/nodc-sweden/nodc-codes/main/src/nodc_codes/CONFIG_FILES/translate_codes_NEW.txt',
+    r'https://raw.githubusercontent.com/nodc-sweden/nodc-codes/main/src/nodc_codes/CONFIG_FILES/translate_codes.txt',
 ]
 
 
 @functools.cache
 def get_translate_codes_object() -> "TranslateCodes":
-    path = CONFIG_DIR / 'translate_codes_NEW.txt'
+    path = CONFIG_DIR / 'translate_codes.txt'
     return TranslateCodes(path)
 
 
@@ -29,10 +30,11 @@ def update_config_files() -> None:
         for url in CONFIG_URLS:
             name = pathlib.Path(url).name
             target_path = CONFIG_DIR / name
-            res = requests.get(url)
-            with open(target_path, 'w', encoding='utf8') as fid:
+            res = requests.get(url, verify=ssl.CERT_NONE)
+            with open(target_path, 'w', encoding='cp1252') as fid:
                 fid.write(res.text)
                 logger.info(f'Config file "{name}" updated from {url}')
     except requests.exceptions.ConnectionError:
         logger.warning('Connection error. Could not update config files!')
+        raise
 
